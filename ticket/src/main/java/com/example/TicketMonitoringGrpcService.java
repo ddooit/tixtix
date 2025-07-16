208c722096d8a643c757408a92ff59e6feeb3450
ddooit <eun.chae.pk@gmail.com>
2025-07-16 12:56:44 +0900

    response 객체 메서드 추출
    가독성..

--- ticket/src/main/java/com/example/TicketMonitoringGrpcService.java
+++ ticket/src/main/java/com/example/TicketMonitoringGrpcService.java
@@ -7,6 +7,8 @@ import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.stream.Stream;
+
 public class TicketMonitoringGrpcService extends TicketingGrpc.TicketingImplBase {
 
     private final static Logger logger = LoggerFactory.getLogger(TicketMonitoringGrpcService.class);
@@ -15,39 +17,27 @@ public class TicketMonitoringGrpcService extends TicketingGrpc.TicketingImplBase
     public void ticketMonitoring(final TicketMonitoringRequest request, final StreamObserver<TicketMonitoringResponse> responseObserver) {
         logger.info("<<< TICKET SERVER >>> ticket monitoring request : {}", request);
 
-        final var first = TicketMonitoringResponse.newBuilder()
-                .setRemainingTicketCount(10_000)
-                .build();
-        responseObserver.onNext(first);
+        Stream.of(ticketMonitoringResponse(10_000),
+                        ticketMonitoringResponse(9_000),
+                        ticketMonitoringResponse(7_000),
+                        ticketMonitoringResponse(500))
+                .forEach(response -> {
+                    try {
+                        Thread.sleep(1_000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
 
-        try {
-            Thread.sleep(1_000);
-            final var second = TicketMonitoringResponse.newBuilder()
-                    .setRemainingTicketCount(9_000)
-                    .build();
-            responseObserver.onNext(second);
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            Thread.sleep(2_000);
-            final var third = TicketMonitoringResponse.newBuilder()
-                    .setRemainingTicketCount(7_000)
-                    .build();
-            responseObserver.onNext(third);
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        final var fourth = TicketMonitoringResponse.newBuilder()
-                .setRemainingTicketCount(500)
-                .build();
-        responseObserver.onNext(fourth);
+                    responseObserver.onNext(response);
+                });
 
         responseObserver.onCompleted();
+    }
+
+    private static TicketMonitoringResponse ticketMonitoringResponse(final int value) {
+        return TicketMonitoringResponse.newBuilder()
+                .setRemainingTicketCount(value)
+                .build();
     }
 
 }
